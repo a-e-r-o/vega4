afa644817524eb1586137070a9fd98800ef2ca44
Aero <aerocloud27@gmail.com>
2025-11-07 00:02:31 +0100

    initial commit

---
+++ VEGA/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using NetCord;
+using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
